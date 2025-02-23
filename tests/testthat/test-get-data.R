58b32ac4d69ce4c056cab51b39e740b550629f12
Garrick Aden-Buie <garrick@adenbuie.com>
2025-02-23 07:56:27 -0500

    tests(get_data_frame): Test all formats

--- tests/testthat/test-get-data.R
+++ tests/testthat/test-get-data.R
@@ -2,9 +2,18 @@ test_that("get_data_frame works", {
   expect_snapshot(cat(get_data_frame(mtcars)))
   expect_snapshot(cat(get_data_frame(mtcars, dims = c(Inf, Inf))))
 
+  expect_snapshot(cat(get_data_frame(mtcars, format = "glimpse")))
+  expect_snapshot(
+    cat(get_data_frame(mtcars, format = "glimpse", dims = c(Inf, Inf)))
+  )
+
   expect_snapshot(cat(get_data_frame(mtcars, format = "print")))
-  expect_snapshot(cat(get_data_frame(mtcars, format = "print", dims = c(Inf, Inf))))
+  expect_snapshot(
+    cat(get_data_frame(mtcars, format = "print", dims = c(Inf, Inf)))
+  )
 
   expect_snapshot(cat(get_data_frame(mtcars, format = "json")))
-  expect_snapshot(cat(get_data_frame(mtcars, format = "json", dims = c(Inf, Inf))))
+  expect_snapshot(
+    cat(get_data_frame(mtcars, format = "json", dims = c(Inf, Inf)))
+  )
 })
