7cc6748db503472ec03ca7ce51eb5947db9020f6
Garrick Aden-Buie <garrick@adenbuie.com>
2025-02-25 11:39:54 -0500

    tests: Use `cli::cat_line()` instead of `cat()`

--- tests/testthat/test-get-env.R
+++ tests/testthat/test-get-env.R
@@ -2,10 +2,10 @@ test_that("btw_this.environment() works", {
   skip_if_not_macos()
   env <- new_environment(list(mtcars = mtcars, boop = "bop"))
 
-  expect_snapshot(cat(btw_this(env)))
-  expect_snapshot(cat(btw_this(env, items = "mtcars")))
-  expect_snapshot(cat(btw_this(env, items = "boop")))
-  expect_snapshot(cat(btw_this(env, items = character(0))))
+  expect_snapshot(cli::cat_line(btw_this(env)))
+  expect_snapshot(cli::cat_line(btw_this(env, items = "mtcars")))
+  expect_snapshot(cli::cat_line(btw_this(env, items = "boop")))
+  expect_snapshot(cli::cat_line(btw_this(env, items = character(0))))
 })
 
 test_that("btw_this.environment() does not support entire namespaces (yet)", {
