7a6e80aae6293a5cb14e439ed5a3c0de998afa87
Garrick Aden-Buie <garrick@adenbuie.com>
2025-03-05 11:18:38 -0500

    docs(btw): Add examples of in-chat usage

--- R/btw.R
+++ R/btw.R
@@ -5,6 +5,37 @@
 #' to a model by concatenating plain-text descriptions of "R stuff", from
 #' data frames to packages to function documentation.
 #'
+#' There are two key ways to use `btw()`:
+#'
+#' 1. Use it interactively at the console to gather information about your
+#'    environment into prompt text that you can paste into the chat interface of
+#'    an LLM, like ChatGPT or Claude. By default, `btw()` copies the prompt to
+#'    the clipboard for you.
+#'
+#'    ```r
+#'    btw(vignette("colwise", "dplyr"), dplyr::across, dplyr::starwars)
+#'    #> ✔ btw copied to the clipboard!
+#'    ```
+#'
+#' 2. Pair `btw()` with [ellmer::Chat] during a chat session to create a prompt
+#'    that includes additional context drawn from your environment and help
+#'    pages.
+#'
+#'    ```r
+#'    library(ellmer)
+#'
+#'    chat <- chat_claude() # requires an Anthropic API key
+#'    chat <- chat_ollama(model = "llama3.1:8b") # requires ollama and a local model
+#'
+#'    chat$chat(btw(
+#'      vignette("colwise", "dplyr"),
+#'      dplyr::across,
+#'      dplyr::starwars,
+#'      "Create a few interesting examples that use `dplyr::across()`",
+#'      "with the `starwars` data set."
+#'    ))
+#'    ```
+#'
 #' @param ... Objects to describe from your R environment. You can pass objects
 #'   themselves, like data frames or functions, or the function also accepts
 #'   output from `btw_tool_*()` functions like
@@ -21,9 +52,21 @@
 #'
 #' btw(btw::btw)
 #'
+#' if (FALSE) {
+#'   # btw() can also be used directly in {ellmer} chats
+#'   library(ellmer)
+#'
+#'   chat <- chat_ollama(model = "llama3.1:8b")
+#'   chat$chat(
+#'     btw(mtcars, "Are there cars with 8 cylinders in this dataset?")
+#'   )
+#' }
+#'
 #' @returns
-#' The combined elements as a string, invisibly. If `clipboard` is `TRUE`, the
-#' result is also written to the system clipboard.
+#' Returns an [ellmer::ContentText] object with the collected prompt. If
+#' `clipboard = TRUE`, the prompt text is copied to the clipboard when the
+#' returned object is printed for the first time (e.g. calling `btw()` without
+#' assignment).
 #'
 #' @export
 btw <- function(..., clipboard = TRUE) {
