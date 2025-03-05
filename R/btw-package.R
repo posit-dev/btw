e765cca3fecfb067016708fc0e65634e32468fea
Garrick Aden-Buie <garrick@adenbuie.com>
2025-03-05 11:32:29 -0500

    chore: small moving around of things

--- R/btw-package.R
+++ R/btw-package.R
@@ -6,10 +6,6 @@
 ## usethis namespace: end
 NULL
 
-# enable usage of <S7_object>@name in package code
-#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
-NULL
-
 utils::globalVariables(c(
   "aliases",
   "capture.output",
