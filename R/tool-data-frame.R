7d799f7986d2106019e261f283d5725dc0a11140
simonpcouch <simonpatrickcouch@gmail.com>
2025-04-29 08:26:50 -0500

    pare back options for data frame `format`s

--- R/tool-data-frame.R
+++ R/tool-data-frame.R
@@ -1,21 +1,17 @@
 #' Describe a data frame in plain text
 #'
 #' @param x A data frame or tibble.
-#' @param format One of `"skim"`, `"glimpse"`, `"print"`, or `"json"`.
+#' @param format One of `"skim"` or `"json"`.
 #' * `"skim"` is the most information-dense format for describing the data. It
 #'   uses and returns the same information as [skimr::skim()] but formatting as
 #'   a JSON object that describes the dataset.
-#' * To glimpse the data column-by-column, use `"glimpse"`. This is
-#'   particularly helpful for getting a sense of data frame column names,
-#'   types, and distributions, when pairings of entries in individual rows
-#'   aren't particularly important.
-#' * To just print out the data frame, use `print()`.
 #' * To get a json representation of the data, use `"json"`. This is
 #'   particularly helpful when the pairings among entries in specific rows
 #'   are important to demonstrate.
-#' @param dims The number of rows and columns to show, as a numeric vector of
-#' length two. For example, the default `dims = c(5, 100)` shows the first 5
-#' rows and 100 columns, whereas `dims = c(Inf, Inf)` would show all of the data.
+#' @param dims For `format = "json"`, the number of rows and columns to show, 
+#' as a numeric vector of length two. For example, the default 
+#' `dims = c(5, 100)` shows the first 5 rows and 100 columns, whereas 
+#' `dims = c(Inf, Inf)` would show all of the data.
 #' @param ... Additional arguments are silently ignored.
 #'
 #' @returns
@@ -24,8 +20,6 @@
 #'
 #' @examples
 #' btw_this(mtcars)
-#'
-#' btw_this(mtcars, format = "print")
 #'
 #' btw_this(mtcars, format = "json")
 #'
@@ -37,7 +31,7 @@
 btw_this.data.frame <- function(
   x,
   ...,
-  format = c("skim", "glimpse", "print", "json"),
+  format = c("skim", "json"),
   dims = c(5, 100)
 ) {
   btw_tool_env_describe_data_frame(x, format = format, dims = dims)
@@ -49,7 +43,7 @@ btw_this.data.frame <- function(
 btw_this.tbl <- function(
   x,
   ...,
-  format = c("skim", "glimpse", "print", "json"),
+  format = c("skim", "json"),
   dims = c(5, 100)
 ) {
   btw_tool_env_describe_data_frame(x, format = format, dims = dims)
@@ -70,7 +64,7 @@ btw_this.tbl <- function(
 #' @export
 btw_tool_env_describe_data_frame <- function(
   data_frame,
-  format = c("skim", "glimpse", "print", "json"),
+  format = c("skim", "json"),
   dims = c(5, 100)
 ) {
   format <- arg_match(format)
@@ -88,7 +82,7 @@ btw_tool_env_describe_data_frame <- function(
     md_table(data_frame)
   }
 
-  if (format %in% c("print", "json")) {
+  if (identical(format, "json")) {
     n_row <- min(dims[1], nrow(data_frame))
     n_col <- min(dims[2], ncol(data_frame))
     data_frame_small <- data_frame[seq_len(n_row), seq_len(n_col), drop = FALSE]
@@ -96,8 +90,6 @@ btw_tool_env_describe_data_frame <- function(
 
   res <- switch(
     format,
-    glimpse = describe_data_frame_glimpse(x = data_frame),
-    print = describe_data_frame_print(x = data_frame_small),
     json = describe_data_frame_json(x = data_frame_small),
     skim = describe_data_frame_skim(data_frame)
   )
@@ -118,11 +110,9 @@ btw_tool_env_describe_data_frame <- function(
       ),
       format = ellmer::type_string(
         paste(
-          "The output format of the data frame: 'skim', 'glimpse', 'print', or 'json'. Default 'skim'.",
+          "The output format of the data frame: 'skim' or 'json'. Default 'skim'.",
           "",
           "* skim: Returns a JSON object with information about every column in the table.",
-          "* glimpse: Returns the number of rows, columns, column names and types and the first values of each column",
-          "* print: Prints the data frame",
           "* json: Returns the data frame as JSON",
           sep = "\n"
         ),
@@ -130,7 +120,7 @@ btw_tool_env_describe_data_frame <- function(
       ),
       dims = ellmer::type_array(
         paste(
-          'Dimensions of the data frame to use for the "print" or "json" format.',
+          'Dimensions of the data frame to use for the "json" format.',
           "A numeric vector of length 2 as number of rows and columns. Default `[5, 100]`."
         ),
         items = ellmer::type_integer(),
@@ -139,20 +129,6 @@ btw_tool_env_describe_data_frame <- function(
     )
   }
 )
-
-describe_data_frame_glimpse <- function(x, x_name) {
-  res <- cli::ansi_strip(capture.output(dplyr::glimpse(x)))
-  as_btw_capture(res)
-}
-
-describe_data_frame_print <- function(x) {
-  withr::local_options(pillar.advice = FALSE, pillar.min_title_chars = Inf)
-
-  res <- cli::ansi_strip(
-    capture.output(tibble::as_tibble(x, width = 1000, n = Inf))
-  )
-  as_btw_capture(res[2:length(res)])
-}
 
 describe_data_frame_json <- function(x) {
   md_code_block("json", as_json_rowwise(x))
--- tests/testthat/test-tool-data-frame.R
+++ tests/testthat/test-tool-data-frame.R
@@ -4,16 +4,6 @@ test_that("btw_this.data.frame() works", {
   expect_snapshot(cli::cat_line(btw_this(mtcars)))
   expect_snapshot(cli::cat_line(btw_this(mtcars, dims = c(Inf, Inf))))
 
-  expect_snapshot(cli::cat_line(btw_this(mtcars, format = "glimpse")))
-  expect_snapshot(
-    cli::cat_line(btw_this(mtcars, format = "glimpse", dims = c(Inf, Inf)))
-  )
-
-  expect_snapshot(cli::cat_line(btw_this(mtcars, format = "print")))
-  expect_snapshot(
-    cli::cat_line(btw_this(mtcars, format = "print", dims = c(Inf, Inf)))
-  )
-
   expect_snapshot(cli::cat_line(btw_this(mtcars, format = "json")))
   expect_snapshot(
     cli::cat_line(btw_this(mtcars, format = "json", dims = c(Inf, Inf)))
