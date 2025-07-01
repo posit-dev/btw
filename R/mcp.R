876f670c087814dbc4ecddaf5282d7266ed4080b
Garrick Aden-Buie <garrick@adenbuie.com>
2025-07-01 16:42:50 -0400

    chore: update to use `tools` consistently with btw now

--- R/mcp.R
+++ R/mcp.R
@@ -1,28 +1,27 @@
-
 #' Start a Model Context Protocol server with btw tools
-#' 
+#'
 #' @description
 #' `btw_mcp_server()` starts an MCP server with tools from [btw_tools()], which
 #' can provide MCP clients like Claude Desktop or Claude Code with additional
-#' context. The function will block the R process it's called in and isn't 
+#' context. The function will block the R process it's called in and isn't
 #' intended for interactive use.
-#' 
+#'
 #' To give the MCP server access to a specific R session, run `btw_mcp_session()`
 #' in that session. If there are no sessions configured, the server will run
 #' the tools in its own session, meaning that e.g. the
 #' `btw_tools(tools = "env")` tools will describe R objects in _that_ R
 #' environment.
-#' 
+#'
 #' @seealso
 #' These functions use [acquaint::mcp_server()] and [acquaint::mcp_session()]
 #' under the hood. To configure arbitrary tools with an MCP client, see the
 #' documentation of those functions.
-#' 
-#' @section Configuration: 
-#' To configure this server with MCP clients, use the command `Rscript` and the 
-#' args `-e "btw::btw_mcp_server()"`. For example, in Claude Desktop's 
+#'
+#' @section Configuration:
+#' To configure this server with MCP clients, use the command `Rscript` and the
+#' args `-e "btw::btw_mcp_server()"`. For example, in Claude Desktop's
 #' configuration format:
-#' 
+#'
 #' ```json
 #' {
 #'   "mcpServers": {
@@ -33,41 +32,36 @@
 #'   }
 #' }
 #' ```
-#' 
+#'
 #' Or, with Claude Code:
-#' 
+#'
 #' ```bash
 #' claude mcp add -s "user" r-acquaint -- Rscript -e "btw::btw_mcp_server()"
 #' ```
-#' 
-#' @inheritParams btw_tools
-#' @param ... Named ellmer tools from [ellmer::tool] to include in addition to 
-#' those specified in `tools`.
+#'
+#' @inheritParams btw_client
 #'
 #' @examples
-#' # Should only be run non-interactively, and 
+#' # Should only be run non-interactively, and
 #' # will block the current R process once called.
 #' if (FALSE) {
 #'   # To start a server with btw tools:
 #'   btw_mcp_server()
-#'  
-#'   # To only do so with a subset of btw's tools, e.g. those 
+#'
+#'   # To only do so with a subset of btw's tools, e.g. those
 #'   # that fetch package documentation:
-#'   btw_mcp_server(tools = "docs")
+#'   btw_mcp_server(tools = btw_tools("docs"))
 #' }
-#' 
-#' # To allow the server to access variables in specific 
+#'
+#' # To allow the server to access variables in specific
 #' # sessions, call `btw_mcp_session()` in that session:
 #' btw_mcp_session()
-#' 
+#'
 #' @name mcp
-#' @export 
-btw_mcp_server <- function(tools = NULL, ...) {
-  # evaluate eagerly so that `btw_tools()` will check `tools`
-  btw_tools <- btw::btw_tools(tools = tools)
-  dots <- dots_list(...)
-  
-  acquaint::mcp_server(tools = utils::modifyList(btw_tools, dots))
+#' @export
+btw_mcp_server <- function(tools = btw_tools()) {
+  tools <- flatten_and_check_tools(tools)
+  acquaint::mcp_server(tools = tools)
 }
 
 #' @rdname mcp
