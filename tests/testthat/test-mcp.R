b6e415f5ebed1e3e7e467c6aa2b6445c3a705a2d
Garrick Aden-Buie <garrick@adenbuie.com>
2025-07-01 16:43:07 -0400

    chore: Use classed error for unmatched btw tool

--- tests/testthat/test-mcp.R
+++ tests/testthat/test-mcp.R
@@ -1,3 +1,6 @@
 test_that("btw_mcp_server errors informatively with bad `tools`", {
-  expect_snapshot(error = TRUE, btw_mcp_server(tools = "bop"))
+  expect_error(
+    class = "btw_unmatched_tool_error",
+    btw_mcp_server(tools = "bop")
+  )
 })
