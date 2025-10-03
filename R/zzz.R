4d3299802935e55bf34697f6553fc0d67c4e3900
Garrick Aden-Buie <garrick@adenbuie.com>
2025-10-03 10:02:42 -0400

    chore: Declare global variables for older R versions

--- R/zzz.R
+++ R/zzz.R
@@ -11,3 +11,17 @@
 # enable usage of <S7_object>@name in package code
 #' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
 NULL
+
+if (getRversion() < "4.3.0") {
+  utils::globalVariables(
+    c(
+      "annotations",
+      "arguments",
+      "description",
+      "extra",
+      "name",
+      "properties",
+      "value"
+    )
+  )
+}
