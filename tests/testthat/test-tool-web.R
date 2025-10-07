1a46e42a0198dbe849d8e8a0e6ae34b7aaffa6b2
Garrick Aden-Buie <garrick@adenbuie.com>
2025-10-07 11:53:57 -0400

    tests(tool-web): Warm up chromote on CI

--- tests/testthat/test-tool-web.R
+++ tests/testthat/test-tool-web.R
@@ -1,3 +1,26 @@
+test_that("ci: prep chromote", {
+  # Try to warm up chromote. IDK why it fails on older versions of R.
+  test_that("Chromote loads", {
+    skip_on_cran()
+    skip_if_not_installed("chromote")
+
+    on_ci <- isTRUE(as.logical(Sys.getenv("CI")))
+    skip_if(!on_ci, "Not on CI")
+
+    # Wrap in a `try()` as the test doesn't matter
+    # Only the action of trying to open chromote matters
+    try({
+      chromote <- utils::getFromNamespace(
+        "default_chromote_object",
+        "chromote"
+      )()
+      chromote$new_session()
+    })
+
+    expect_true(TRUE)
+  })
+})
+
 test_that("has_chromote() detects chromote availability", {
   # When chromote is installed
   local_mocked_bindings(
